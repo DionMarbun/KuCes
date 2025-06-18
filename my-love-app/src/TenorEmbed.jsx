d76ed9525422fe205622566cfcdba7cb654dfc0b
dionmarbun23ti@mahasiswa.pcr.ac.id <dionmarbun23ti@mahasiswa.pcr.ac.id>
2025-06-19 00:03:51 +0700

    pesan

---
+++ my-love-app/src/TenorEmbed.jsx
@@ -0,0 +1,23 @@
+// TenorEmbed.jsx
+import { useEffect } from 'react';
+
+function TenorEmbed({ postId }) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://tenor.com/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div
+      className="tenor-gif-embed"
+      data-postid={postId}
+      data-share-method="host"
+      data-aspect-ratio="1"
+      data-width="100%"
+    ></div>
+  );
+}
+
+export default TenorEmbed;
